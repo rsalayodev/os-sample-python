387b1e7ce3fc4f872eae49882a2443e158170c08
Ryan Joseph Salayo <ryan@Ryans-MacBook-Pro.local>
2019-10-19 16:24:10 +0800

    further changes

--- wsgi.py
+++ wsgi.py
@@ -3,7 +3,7 @@ application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "Hello World! Openshift Testing!!sdsd!!!!"
+    return "Hello World! Openshift Testing!!sdssdasdasdd!!!!"
 
 if __name__ == "__main__":
     application.run()
