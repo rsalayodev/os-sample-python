ca7c89407f9c21da800d5509b01f7c160e87aa75
Ryan Joseph Salayo <ryan@Ryans-MacBook-Pro.local>
2019-10-19 16:20:37 +0800

    Initial Commit

--- wsgi.py
+++ wsgi.py
@@ -3,7 +3,7 @@ application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "Hello World! Openshift Testing"
+    return "Hello World! Openshift Testing!!sdsd!!!!"
 
 if __name__ == "__main__":
     application.run()
